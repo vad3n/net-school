ec20a658a605ba0b6f0f06fae1972860d30cd0a4
vad3n <veselin.ym86@gmail.com>
2024-04-24 14:52:53 +0300

    Changes to Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,8 +5,17 @@ int result = 0;
 
 for (int i = 0; i < input.Length; i++)
 {
-    result += input[i] - '0';
+    result += (input[i] - '0') * (10 - i);
+    Console.WriteLine(result);
 }
 
-int digit = 11 - result % 11;
+int digit = 0;
+
+if (result % 11 != 0)
+{
+    digit = 11 - result % 11;
+}
+
+Console.WriteLine(result);
+
 Console.WriteLine(input + (digit == 10 ? "X" : digit.ToString()));
