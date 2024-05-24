fbc87ecd2097640ca284d04058d5b12dcd950602
vad3n <veselin.ym86@gmail.com>
2024-05-24 15:27:20 +0300

    Changes to Task11

--- Task11/Catalog.cs
+++ Task11/Catalog.cs
@@ -1,59 +1,95 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace Task11
 {
-    public class Catalog
+    public class Catalog : IEnumerable
     {
-        private IDictionary<string, Book> _books;
+        private IList<Tuple<string, Book>> _books;
 
         private bool isValidISBN(string isbn)
         {
-            return (isbn.Length == 17&& Regex.IsMatch(isbn, @"\d{3}-\d{1}-\d{2}-\d{6}-\d{1}")) || (isbn.Length == 13 && Regex.IsMatch(isbn, @"\d{13}"));
+            return (isbn.Length == 17 && Regex.IsMatch(isbn, @"\d{3}-\d{1}-\d{2}-\d{6}-\d{1}")) || (isbn.Length == 13 && Regex.IsMatch(isbn, @"\d{13}"));
         }
 
         public Catalog()
         {
-            _books = new Dictionary<string, Book>();
+            _books = new List<Tuple<string, Book>>();
         }
 
-        public void AddBook(string isbn, Book book)
+        public Book this[string isbn]
         {
-            if (!isValidISBN(isbn))
+            get
             {
-                throw new ArgumentException("Invalid format for ISBN.");
+                if (!isValidISBN(isbn))
+                {
+                    throw new ArgumentException("Invalid format for ISBN.");
+                }
+
+                return _books.First(t => t.Item1.Equals(isbn)).Item2;
             }
 
-            if (_books.ContainsKey(isbn))
+            set
             {
-                throw new ArgumentException("Book with ISBN already exists.");
+                if (!isValidISBN(isbn))
+                {
+                    throw new ArgumentException("Invalid format for ISBN.");
+                }
+
+                if (_books.Any(t => t.Item1.Equals(isbn)))
+                {
+                    throw new ArgumentException("Book with ISBN already exists.");
+                }
+
+                _books.Add(Tuple.Create(isbn, value));
             }
-
-            _books.Add(isbn, book);
-        }
-
-        public Book GetBook(string isbn)
-        {
-            if (!isValidISBN(isbn))
-            {
-                throw new ArgumentException("Invalid format for ISBN.");
-            }
-
-            return _books[isbn];
         }
 
         public ISet<Book> GetSortedBooks()
         {
-            return _books.Values.OrderBy(b => b.Title).ToHashSet();
+            return _books.Select(t => t.Item2).OrderBy(b => b.Title).ToHashSet();
         }
 
         public ISet<Book> GetBooksByAuthor(string author)
         {
-            return _books.Values.Where(b => b.Authors.Contains(author)).OrderBy(b => b.PublicationDate).ToHashSet();
+            return _books.Select(t => t.Item2).Where(b => b.Authors.Contains(author)).OrderBy(b => b.PublicationDate).ToHashSet();
         }
 
-        public (string, int) GetAuthorBooksCount(string author)
+        public ISet<Tuple<string, int>> GetAuthorBooksCount()
         {
-            return (author, _books.Values.Where(b => b.Authors.Contains(author)).Count());
+            ISet<string> authors = new HashSet<string>();
+
+            foreach (var tuple in _books)
+            {
+                foreach (var author in tuple.Item2.Authors)
+                {
+                    authors.Add(author);
+                }
+            }
+
+            ISet<Tuple<string, int>> result = new HashSet<Tuple<string, int>>();
+
+            foreach (var author in authors)
+            {
+                int count = 0;
+
+                foreach (var tuple in _books)
+                {
+                    if (tuple.Item2.Authors.Contains(author))
+                    {
+                        count++;
+                    }
+                }
+
+                result.Add(Tuple.Create(author, count));
+            }
+
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)_books).GetEnumerator();
         }
     }
 }
--- Task11/Program.cs
+++ Task11/Program.cs
@@ -29,39 +29,46 @@ Book book4 = new Book("H Book", new DateOnly(2012, 8, 28), set4);
 Book book5 = new Book("G Book", new DateOnly(2015, 12, 8), set5);
 
 Catalog catalog = new Catalog();
-catalog.AddBook("123-5-28-456792-5", book1);
-catalog.AddBook("123-5-28-456792-8", book2);
-catalog.AddBook("1247984567927", book3);
-catalog.AddBook("389-5-28-856792-5", book4);
-catalog.AddBook("450-5-28-370916-4", book5);
+catalog["123-5-28-456792-5"] = book1;
+catalog["123-5-28-456792-8"] = book2;
+catalog["1247984567927"] = book3;
+catalog["389-5-28-856792-5"] = book4;
+catalog["450-5-28-370916-4"] = book5;
 
 Book test = new Book();
-//catalog.AddBook("71-2-78-459602-4", test);
-//catalog.AddBook("7145-2-78-459602-4", test);
-//catalog.AddBook("714--78-459602-4", test);
-//catalog.AddBook("714-24-78-459602-4", test);
-//catalog.AddBook("714-2-7-459602-4", test);
-//catalog.AddBook("714-2-788-459602-4", test);
-//catalog.AddBook("714-2-78-45960-4", test);
-//catalog.AddBook("714-2-78-4596011-4", test);
-//catalog.AddBook("714-2-78-459601-", test);
-//catalog.AddBook("714-2-78-459601-45", test);
-//catalog.AddBook("7142-788-459601-4", test);
-//catalog.AddBook("714--2-78-459601-4", test);
-//catalog.AddBook("714-278-459601-4", test);
-//catalog.AddBook("714-2--78-459601-4", test);
-//catalog.AddBook("714-2-78459601-4", test);
-//catalog.AddBook("714-2-78--459601-4", test);
-//catalog.AddBook("714-2-78-4596014", test);
-//catalog.AddBook("714-2-78-459601--4", test);
-//catalog.AddBook("7A14-2-78-459601-4", test);
-//catalog.AddBook("714-2c-78-459601-4", test);
-//catalog.AddBook("714-2-78-459G601-4", test);
-//catalog.AddBook("714-2-78-459601-4p", test);
-//catalog.AddBook("714278459601", test);
-//catalog.AddBook("71427845960148", test);
+//catalog["71-2-78-459602-4"] = test;
+//catalog["7145-2-78-459602-4"] = test;
+//catalog["714--78-459602-4"] = test;
+//catalog["714-24-78-459602-4"] = test;
+//catalog["714-2-7-459602-4"] = test;
+//catalog["714-2-788-459602-4"] = test;
+//catalog["714-2-78-45960-4"] = test;
+//catalog["714-2-78-4596011-4"] = test;
+//catalog["714-2-78-459601-"] = test;
+//catalog["714-2-78-459601-45"] = test;
+//catalog["7142-788-459601-4"] = test;
+//catalog["714--2-78-459601-4"] = test;
+//catalog["714-278-459601-4"] = test;
+//catalog["714-2--78-459601-4"] = test;
+//catalog["714-2-78459601-4"] = test;
+//catalog["714-2-78--459601-4"] = test;
+//catalog["714-2-78-4596014"] = test;
+//catalog["714-2-78-459601--4"] = test;
+//catalog["7A14-2-78-459601-4"] = test;
+//catalog["714-2c-78-459601-4"] = test;
+//catalog["714-2-78-459G601-4"] = test;
+//catalog["714-2-78-459601-4p"] = test;
+//catalog["714278459601"] = test;
+//catalog["71427845960148"] = test;
+
+Console.WriteLine(catalog["1247984567927"]);
+//Console.WriteLine(catalog["124-7-98-456792-7"]);
 
 ISet<Book> sorted = catalog.GetSortedBooks();
 ISet<Book> author = catalog.GetBooksByAuthor("Author2");
-(string, int) count = catalog.GetAuthorBooksCount("Author3");
-Console.WriteLine(count);
+ISet<Tuple<string, int>> count = catalog.GetAuthorBooksCount();
+
+foreach (var item in count)
+{
+    Console.WriteLine(item.Item1 + " " + item.Item2);
+}
